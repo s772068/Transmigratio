fbc13ec1e22050a2f2bafd89d6d19501ab804861
GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
2024-07-07 16:35:59 +0300

    Tutorial ver 0.1
    + Префабы основных попапов
    + Черновая версия туториала
    - Изменения в скрипте HUD

--- Assets/09/09_Scripts/Controllers/HUD.cs
+++ Assets/09/09_Scripts/Controllers/HUD.cs
@@ -1,7 +1,4 @@
-using UnityEngine.Localization.Settings;
-using UnityEngine.UI;
 using UnityEngine;
-using TMPro;
 
 /// <summary>
 /// Интерфейс, всплывающие окна и тд
@@ -10,18 +7,6 @@ public class HUD : MonoBehaviour
 {
     [SerializeField] private Messanger messanger;
 
-    [Header("Tutorial")]
-    public Transform tutorialWhole;     //объект-родитель всех обучающих объектов
-    public Transform welcomePopup;      //привествие с портретом бога
-    public Transform surePopup;         //окно подтверждения отмены обучения
-    public TMP_Text welcomePopupText;   //текст в привественном окне
-    public Button welcomeContinueBtn;   //кнопка продолжения обучения
-    public Button cancelTutor;          //кнопка "Да" в подтверждающем окошке, при нажатии отменяется обучение
-    public Transform otherPopup;        //окошко для остальных обучающих сообщений
-    public TMP_Text otherPopupText;     //текст в otherPopup
-    public Button okBtn;                //кнопка "ок" в otherPopup
-    public bool tutorCanceled;  //флаг, указывающий на то, отменён ли тутор
-    
     public bool isShowMigration = true;
 
     [Header("Region Details")]
@@ -36,29 +21,6 @@ public class HUD : MonoBehaviour
     private void OnShowMessage(string message) {
         messanger.gameObject.SetActive(true);
         messanger.Message = message;
-    }
-
-    public void StartTutorial() {
-        tutorialWhole.gameObject.SetActive(true);
-        welcomePopup.gameObject.SetActive(true);
-        welcomePopupText.text = LocalizationSettings.StringDatabase.GetLocalizedString("TransmigratioLocalizationTable", "Tutorial0");
-        welcomeContinueBtn.onClick.AddListener(() => ShowTutorPopup("Tutorial1"));
-        cancelTutor.onClick.AddListener(StopTutorial);
-    }
-
-    public void StopTutorial() {
-        //welcomePopup.gameObject.SetActive(false);
-        //surePopup.gameObject.SetActive(false);
-        tutorialWhole.gameObject.SetActive(false);
-        tutorCanceled = true;
-    }
-
-    public void ShowTutorPopup(string tutorKey) {
-        if (tutorCanceled) return;
-        otherPopupText.text = LocalizationSettings.StringDatabase.GetLocalizedString("TransmigratioLocalizationTable", tutorKey);
-        tutorialWhole.gameObject.SetActive(true);
-        otherPopup.gameObject.SetActive(true);
-        welcomePopup.gameObject.SetActive(false);
     }
 
     public void ShowMigration() {
--- Assets/09/UI/Scripts/Tutorial.cs
+++ Assets/09/UI/Scripts/Tutorial.cs
@@ -1,13 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Localization.Components;
 
 public class Tutorial : MonoBehaviour
 {
+    [SerializeField] private bool _suggestTutorial = true;
+    [SerializeField] private GameObject _zones;
+    [SerializeField] private SerializedDictionary<string, GameObject> _uiZones;
+    private bool _tutorialEnded = false;
 
-    private void Start()
+    [Header("Tutorial Steps")]
+    [SerializeField] private GameObject _welcome;
+    [SerializeField] private GameObject _gameGoal;
+    [SerializeField] private GameObject _infoPanel;
+    [SerializeField] private GameObject _startGame;
+    [SerializeField] private GameObject _gameEvent;
+    [SerializeField] private GameObject _markers;
+    [SerializeField] private GameObject _layers;
+    LocalizeStringEvent test;
+
+    private void OnEnable()
     {
         
     }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void Start()
+    {
+        if (_suggestTutorial)
+        {
+            _zones.SetActive(true);
+            _welcome.SetActive(true);
+        }
+        else
+            _tutorialEnded = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_tutorialEnded)
+            gameObject.SetActive(false);
+    }
+
+    public void GameGoal(bool open) => _gameGoal.SetActive(open);
+
+    public void InfoPanel(bool open)
+    {
+        if (open)
+        {
+            _infoPanel.SetActive(true);
+            _uiZones["InfoPanel"].SetActive(true);
+        }
+        else
+        {
+            _infoPanel.SetActive(false);
+            _uiZones["InfoPanel"].SetActive(false);
+        }
+    }
+
+    public void GameStarted(bool open)
+    {
+        if (open)
+        {
+            _startGame.SetActive(true);
+            _uiZones["Timer"].SetActive(true);
+        }
+        else
+        {
+            _startGame.SetActive(false);
+            _uiZones["Timer"].SetActive(false);
+        }
+    }
+
+    public void GameEvent(bool open)
+    {
+        if (open)
+        {
+            _gameEvent.SetActive(true);
+            _uiZones["Event"].SetActive(true);
+        }
+        else
+        {
+            _gameEvent.SetActive(false);
+            _uiZones["Event"].SetActive(false);
+        }
+    }
+
+    public void Skip() => _tutorialEnded = true;
 }
