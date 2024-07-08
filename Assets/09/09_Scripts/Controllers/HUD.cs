89f5695cd3b1dd35a8518bdfe4d46eb4ad2a08b3
GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
2024-07-08 14:42:16 +0300

    Squashed commit of the following:
    commit 20a746e3ea6eb0b4bf6754970665de2e9808d706
    Merge: 6d09f1f 4c45655
    Author: GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
    Date:   Mon Jul 8 14:17:10 2024 +0300
    
        Merge branch 'secondary' into Tutorial
    
    commit 6d09f1f48987447a66d7002b7a5a854ec5b3eef4
    Author: GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
    Date:   Mon Jul 8 13:50:28 2024 +0300
    
        Tutorial ver 0.2
    
        Полностью рабочий туториал, с незначительными недочетами.
    
    commit fbc13ec1e22050a2f2bafd89d6d19501ab804861
    Author: GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
    Date:   Sun Jul 7 16:35:59 2024 +0300
    
        Tutorial ver 0.1
    
        + Префабы основных попапов
        + Черновая версия туториала
        - Изменения в скрипте HUD
    
    commit 09865bc75eb75ab80fc0bf8b8e434bae8f67d1f4
    Author: GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
    Date:   Fri Jul 5 21:31:47 2024 +0300
    
        Materials
    
        + Материалы для реверсивной маски
    
    commit 9fa801297757de05914d8a210d0ba8854e47e8ca
    Author: GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
    Date:   Thu Jul 4 15:28:36 2024 +0300
    
        Init

--- Assets/09/09_Scripts/Controllers/HUD.cs
+++ Assets/09/09_Scripts/Controllers/HUD.cs
@@ -1,27 +1,15 @@
-using UnityEngine.Localization.Settings;
-using UnityEngine.UI;
+using System;
 using UnityEngine;
-using TMPro;
 
 /// <summary>
 /// Интерфейс, всплывающие окна и тд
 /// </summary>
 public class HUD : MonoBehaviour
 {
+    public static event Action<bool> EventRegionPanelOpen;
+
     [SerializeField] private Messanger messanger;
 
-    [Header("Tutorial")]
-    public Transform tutorialWhole;     //объект-родитель всех обучающих объектов
-    public Transform welcomePopup;      //привествие с портретом бога
-    public Transform surePopup;         //окно подтверждения отмены обучения
-    public TMP_Text welcomePopupText;   //текст в привественном окне
-    public Button welcomeContinueBtn;   //кнопка продолжения обучения
-    public Button cancelTutor;          //кнопка "Да" в подтверждающем окошке, при нажатии отменяется обучение
-    public Transform otherPopup;        //окошко для остальных обучающих сообщений
-    public TMP_Text otherPopupText;     //текст в otherPopup
-    public Button okBtn;                //кнопка "ок" в otherPopup
-    public bool tutorCanceled;  //флаг, указывающий на то, отменён ли тутор
-    
     public bool isShowMigration = true;
 
     [Header("Region Details")]
@@ -38,29 +26,6 @@ public class HUD : MonoBehaviour
         messanger.Message = message;
     }
 
-    public void StartTutorial() {
-        tutorialWhole.gameObject.SetActive(true);
-        welcomePopup.gameObject.SetActive(true);
-        welcomePopupText.text = LocalizationSettings.StringDatabase.GetLocalizedString("TransmigratioLocalizationTable", "Tutorial0");
-        welcomeContinueBtn.onClick.AddListener(() => ShowTutorPopup("Tutorial1"));
-        cancelTutor.onClick.AddListener(StopTutorial);
-    }
-
-    public void StopTutorial() {
-        //welcomePopup.gameObject.SetActive(false);
-        //surePopup.gameObject.SetActive(false);
-        tutorialWhole.gameObject.SetActive(false);
-        tutorCanceled = true;
-    }
-
-    public void ShowTutorPopup(string tutorKey) {
-        if (tutorCanceled) return;
-        otherPopupText.text = LocalizationSettings.StringDatabase.GetLocalizedString("TransmigratioLocalizationTable", tutorKey);
-        tutorialWhole.gameObject.SetActive(true);
-        otherPopup.gameObject.SetActive(true);
-        welcomePopup.gameObject.SetActive(false);
-    }
-
     public void ShowMigration() {
         if (!isShowMigration) return;
         //migration
@@ -69,5 +34,6 @@ public class HUD : MonoBehaviour
     public void ShowRegionDetails(int region) {
         regionDetails.regionID = region;
         regionDetails.gameObject.SetActive(true);
+        EventRegionPanelOpen?.Invoke(true);
     }
 }
