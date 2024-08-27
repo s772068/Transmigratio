f34f8bcf6f4045127ffa02386688b9e34637cf7f
NyaGames <103975161+zxc101674@users.noreply.github.com>
2024-08-27 14:55:42 +0300

    Created Scroller

---
+++ Assets/1 - Transmigratio/Scripts/UI/ScrollView/Element.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace UI.ScrollVew {
+    public class Element : MonoBehaviour {
+        public int index;
+    }
+}
