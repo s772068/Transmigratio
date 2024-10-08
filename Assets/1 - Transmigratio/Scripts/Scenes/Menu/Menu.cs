4ca1c854d75df1b33f16061d8298a45d37885255
GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
2024-10-08 22:14:40 +0300

    Update

--- Assets/1 - Transmigratio/Scripts/Scenes/Menu/Menu.cs
+++ Assets/1 - Transmigratio/Scripts/Scenes/Menu/Menu.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public void OpenScene(int index) => SceneManager.LoadScene(index);
+
+    public void OpenScene(int index)
+    {
+        StartCoroutine(DebugLoad(index));
+    }
+
+    private IEnumerator DebugLoad(int index)
+    {
+        var op = SceneManager.LoadSceneAsync(index);
+        op.allowSceneActivation = true;
+        while (!op.isDone)
+        {
+            print("progress: " + op.progress);
+            yield return null;
+        }
+    }
 }
